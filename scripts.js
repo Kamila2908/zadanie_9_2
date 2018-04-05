caa17717b1e47d06b32c6990700494d581d16304
Kamila2908 <dudkiewicz.kamila@yahoo.com>
2018-04-05 21:16:10 +0200

    Completed task

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+var newName = 'Czarek'
+
+if (allNames.indexOf(newName) === -1) {
+        var x = allNames.push(newName);
+    } else false
+
+    console.log(allNames)
